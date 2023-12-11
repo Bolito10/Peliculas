8528a93f0c31c711fd992849d3e00bc1a433e9bb
José Luis Ibarra Llamas <luisitoibarra@live.com.mx>
2023-12-11 16:07:58 -0600

    subida api solo para peliculas

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('peliculas', [PelisController::class, 'index'])->name('peliculas.index');
+
+Route::get('peliculas/create', [PelisController::class, 'create'])->name('peliculas.create');
